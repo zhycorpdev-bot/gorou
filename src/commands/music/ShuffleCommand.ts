47b0d56433bea44811e8be14679cfc54fed6a80c
KurokuTetsuya <perumnas.good@gmail.com>
2021-09-20 11:34:19 +0700

    feat(music): added shuffle command

---
+++ src/commands/music/ShuffleCommand.ts
@@ -0,0 +1,35 @@
+import { BaseCommand } from "../../structures/BaseCommand";
+import { CommandContext } from "../../structures/CommandContext";
+import { createEmbed } from "../../utils/createEmbed";
+import { DefineCommand } from "../../utils/decorators/DefineCommand";
+import { isMemberInVoiceChannel, isMemberVoiceChannelJoinable, isMusicPlaying, isSameVoiceChannel } from "../../utils/decorators/MusicHelpers";
+
+@DefineCommand({
+    aliases: [],
+    cooldown: 3,
+    description: "Shuffle current queue",
+    name: "shuffle",
+    slash: {
+        options: []
+    },
+    usage: "{prefix}shuffle"
+})
+export class ShuffleCommand extends BaseCommand {
+    @isMusicPlaying()
+    @isMemberInVoiceChannel()
+    @isMemberVoiceChannelJoinable()
+    @isSameVoiceChannel()
+    public async execute(ctx: CommandContext): Promise<any> {
+        if (ctx.isInteraction() && !ctx.deferred) await ctx.deferReply();
+        await ctx.guild!.music.player!.queue.shuffle();
+        const msg = await ctx.send({
+            embeds: [
+                createEmbed("info", "Shuffled current queue", true)
+            ]
+        });
+        if (ctx.channel!.id === ctx.guild?.music.playerMessage?.channelId) {
+            setTimeout(() => msg.delete().catch(() => null), 5000);
+        }
+        await ctx.guild?.music.updatePlayerEmbed();
+    }
+}
