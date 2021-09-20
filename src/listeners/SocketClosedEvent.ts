29bad670e553493c2bc78cec70eb1128071b25cb
KurokuTetsuya <perumnas.good@gmail.com>
2021-09-20 18:09:09 +0700

    feat(SocketClosedEvent): temp fix for paused when moved

--- src/listeners/SocketClosedEvent.ts
+++ src/listeners/SocketClosedEvent.ts
@@ -1,10 +1,15 @@
-import { Player } from "erela.js";
+import { Player, WebSocketClosedEvent } from "erela.js";
 import { BaseListener } from "../structures/BaseListener";
 import { DefineListener } from "../utils/decorators/DefineListener";
 
+const allowedOpCodes = [4006, 4015, 4011, 4012];
+
 @DefineListener("socketClosed", "erela")
 export class SocketClosedEvent extends BaseListener {
-    public async execute(player: Player): Promise<void> {
-        if (player.paused) player.pause(false);
+    public async execute(player: Player, payload: WebSocketClosedEvent): Promise<void> {
+        if (allowedOpCodes.includes(payload.code)) {
+            setTimeout(() => player.pause(true), 100);
+            setTimeout(() => player.pause(false), 300);
+        }
     }
 }
