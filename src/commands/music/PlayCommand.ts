15f8c6fee61885791bc5ea44c32be076828c3c51
KurokuTetsuya <perumnas.good@gmail.com>
2021-09-24 10:21:55 +0700

    fix(MessageCreateEvent): slice the query

--- src/commands/music/PlayCommand.ts
+++ src/commands/music/PlayCommand.ts
@@ -110,13 +110,10 @@ export class PlayCommand extends BaseCommand {
         } else {
             await ctx.guild!.music.player!.queue.add(response.tracks[0]);
             // Identify if the command is being runned by another command (select menu)
-            if (!ctx.additionalArgs.get("values")) {
-                const msg = await ctx.send({
+            if (!ctx.additionalArgs.get("values") && !ctx.additionalArgs.get("fromRequesterChannel")) {
+                await ctx.send({
                     embeds: [createEmbed("info", `✅ Track **[${response.tracks[0].title}](${response.tracks[0].uri})** has been added to the queue`).setThumbnail(response.tracks[0].thumbnail!)]
                 });
-                if (fromRequester) {
-                    setTimeout(() => msg.delete().catch(() => null), 5000);
-                }
             }
         }
         await ctx.guild!.music.updatePlayerEmbed();
