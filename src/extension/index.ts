f55a470506b85508a4e2ccdde8607a08877f3040
KurokuTetsuya <perumnas.good@gmail.com>
2021-11-22 21:10:21 +0700

    feat(extension): use `BotClient#queue`

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -4,6 +4,6 @@ import { Guild } from "discord.js";
 Reflect.defineProperty(Guild.prototype, "music", {
     get() {
         // @ts-expect-error-next-line
-        return this.client._music.fetch(this.id);
+        return this.client.queue.fetch(this.id);
     }
 });
